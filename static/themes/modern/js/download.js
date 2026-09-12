2cfb1af360cd49156f5bc1a4fdebcf56365bf463
apicalshark <58538165+apicalshark@users.noreply.github.com>
2026-09-12 20:01:20 +0800

    Add license comment to download.js

--- static/themes/modern/js/download.js
+++ static/themes/modern/js/download.js
@@ -1,3 +1,5 @@
+/* @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0 */
+
 /* Floating download dialog for MikuInvidious.
  *
  * Intercepts the video / listen download forms: instead of opening a blank
